23a2d0c7d1cab5c2a55b8e637004abb8a8445bcf
Rodrigo Dalmas <rodrigodalmas@gmail.com>
2025-05-08 14:24:25 +0100

    Initial Commit
    Setup a React + TS application using Vite, with SPA rendering

--- src/catchall.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-    return (
-      <div>
-        <h1>Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <a href="/list">Go back to the home page</a>
-      </div>
-    );
-  }
--- src/pages/PlayersList.tsx
+++
@@ -1,8 +0,0 @@
-export default function List () {
-    return (
-        <div>
-            <h1>List</h1>
-            <p>This is the list page.</p>
-        </div>
-    );
-}
